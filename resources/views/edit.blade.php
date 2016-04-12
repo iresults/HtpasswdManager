98f3c2f4f6477989cf394f662c911292831ba8ab
Daniel Corn <d@cundd.net>
2016-04-12 17:19:45 +0200

    Enhanced the templates

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,7 +11,10 @@
     </form>
 
     <div class="row">
-        <div class="col-xs-3 col-xs-offset-9">
+        <div class="col-md-3 col-md-offset-2">
+            <a class="btn btn-secondary btn-block" href="{{ ir_url('users') }}">Benutzerliste</a>
+        </div>
+        <div class="col-md-3 col-md-offset-4">
             <a class="btn btn-danger btn-block" href="{{ ir_url('user/delete', ['user' => $user->getUsername()]) }}"><i class="fa fa-trash-o" aria-hidden="true"></i> Löschen</a>
         </div>
     </div>
--- resources/views/partials/properties.blade.php
+++ resources/views/partials/properties.blade.php
@@ -1,18 +1,18 @@
 <div class="form-group row">
-    <label for="username" class="col-sm-2 form-control-label">Benutzername</label>
-    <div class="col-sm-10">
+    <label for="username" class="col-md-2 form-control-label">Benutzername</label>
+    <div class="col-md-10">
         <input type="text" class="form-control" id="username" name="username" placeholder="Username"
                value="{{ $username or '' }}">
     </div>
 </div>
 <div class="form-group row">
-    <label for="password" class="col-sm-2 form-control-label">Password</label>
-    <div class="col-sm-10">
+    <label for="password" class="col-md-2 form-control-label">Password</label>
+    <div class="col-md-10">
         <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
     </div>
 </div>
 <div class="form-group row">
-    <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-secondary">Speichern</button>
+    <div class="col-md-offset-2 col-md-10">
+        <button type="submit" class="btn btn-secondary btn-block">Speichern</button>
     </div>
 </div>
